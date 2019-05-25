c6015cf85d8638e1ae598d2de20686e15c1e6fc7
Ali Besharati <1368.ali.b@gmail.com>
2019-05-25 17:57:46 +0430

    ثبت اطلاعات کارفرما _مایگریشن

--- DAL/Migrations/20190525132639_add_some_proprty_to_cotractor.Designer.cs
+++ DAL/Migrations/20190525132639_add_some_proprty_to_cotractor.Designer.cs
@@ -4,14 +4,16 @@ using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(TaxiContext))]
-    partial class TaxiContextModelSnapshot : ModelSnapshot
+    [Migration("20190525132639_add_some_proprty_to_cotractor")]
+    partial class add_some_proprty_to_cotractor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
